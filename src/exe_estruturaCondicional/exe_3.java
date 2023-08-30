e79f7b26ec07e89c3d850733879e76db5777b88d
brun1nho <bruno.p.cardoso@hotmail.com>
2023-08-30 13:47:11 -0300

    Exercícios iniciais em Java

---
+++ src/exe_estruturaCondicional/exe_3.java
@@ -0,0 +1,22 @@
+package exe_estruturaCondicional;
+
+import java.util.Scanner;
+public class exe_3 {
+
+    public static void main(String... args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a % b == 0 || b % a == 0) {
+            System.out.println("São Multiplos");
+        }
+        else{
+            System.out.println("Não são Multiplos");
+        }
+
+        sc.close();
+    }
+}
